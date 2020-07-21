6a38498e2742639dcfe7ed20ec8c50e8cdf1f290
Gregory Casamento <greg.casamento@gmail.com>
2020-07-20 22:53:36 -0400

    Initial Commit

---
+++ NSAlert_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSAlert_test
+//
+//  Created by Gregory Casamento on 7/20/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
